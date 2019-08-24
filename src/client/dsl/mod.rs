18bc5f08bb81399f6f86bb757a5d5aeb8d4cd41e
Michael Mitteregger <michael.mitteregger@gmx.at>
2019-08-24 21:21:40 +0200

    Re-export metadata and stub_import functions

--- src/client/dsl/mod.rs
+++ src/client/dsl/mod.rs
@@ -1,11 +1,13 @@
+pub use crate::client::dsl::count::*;
 pub use crate::client::dsl::matching::*;
-pub use crate::client::dsl::stubbing::*;
 pub use crate::client::dsl::request::*;
 pub use crate::client::dsl::response::*;
-pub use crate::client::dsl::count::*;
+pub use crate::client::dsl::stubbing::*;
+pub use crate::common::metadata;
+pub use crate::stubbing::stub_import;
 
+mod count;
 mod matching;
-mod stubbing;
 mod request;
 mod response;
-mod count;
+mod stubbing;
