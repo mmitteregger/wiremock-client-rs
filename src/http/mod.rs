2d810bb80247f4d0238525b732f66128d7d358a9
Michael Mitteregger <michael.mitteregger@gmx.at>
2019-08-24 18:16:15 +0200

    Re-export StatusCode and the header module from the http crate in the http module

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,12 +1,14 @@
+pub use http::{header, StatusCode};
+
+pub use crate::http::body::Body;
+pub use crate::http::chunked_dribble_delay::ChunkedDribbleDelay;
+pub use crate::http::cookie::Cookie;
+pub use crate::http::delay_distribution::DelayDistribution;
+pub use crate::http::fault::Fault;
+pub use crate::http::logged_response::LoggedResponse;
+pub use crate::http::query_parameter::QueryParameter;
 pub use crate::http::request_method::RequestMethod;
 pub use crate::http::response_definition::ResponseDefinition;
-pub use crate::http::body::Body;
-pub use crate::http::delay_distribution::DelayDistribution;
-pub use crate::http::chunked_dribble_delay::ChunkedDribbleDelay;
-pub use crate::http::fault::Fault;
-pub use crate::http::cookie::Cookie;
-pub use crate::http::query_parameter::QueryParameter;
-pub use crate::http::logged_response::LoggedResponse;
 
 mod request_method;
 mod response_definition;
